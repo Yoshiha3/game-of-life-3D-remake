372e2b07d05499f2fb4b000bf1496fd81e04925f
Yoshiha <haru12292997@gmail.com>
2026-06-09 12:04:59 +0900

    feat: 初期キューブのサイズをランダムに選ぶように変更

--- scripts/main.js
+++ scripts/main.js
@@ -6,12 +6,12 @@ const height = 20;
 const depth = 20;
 const gameOfLife = new GameOfLife(width, height, depth);
 
-const cubeSize = 6;
+const cubeSize = 6 + Math.floor(Math.random() * 7) * 2; // ある程度世代が続く、初期キューブの大きさ（6~18の偶数）からランダムで決定
 gameOfLife.createCube(
   Math.floor(width / 2) - Math.floor(cubeSize / 2),
   Math.floor(height / 2) - Math.floor(cubeSize / 2),
   Math.floor(depth / 2) - Math.floor(cubeSize / 2),
   cubeSize
 )
-gameOfLife.startUpdate();
+gameOfLife.startUpdate(10);
 gameOfLife.startAnimation();
